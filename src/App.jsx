aa499920027c5d31a360c6b39195c678e173bd49
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-02-21 23:06:43 +0530

    init✨

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+
+  return (
+    <div>
+      <ProductCard name="JBL Charge Mini 3" price="3250/-" description="Portable Wireless Bluetooth Speaker With FM Radio JBL" photoURL="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp" />
+      <ProductCard name="Wireless Bluetooth Headphones" price="3900/-" description="P47 Bluetooth 4.1 Headphone Wireless Headband Earphone Hands Free Music Headset With MF/TF & AUX ports , P47 Cat Ears" photoURL="https://img.drz.lazcdn.com/static/lk/p/b2a509263c206e69a6aef620d2579aa2.jpg_720x720q80.jpg_.webp" />
+
+    </div>
+  )
+}
+
+export default App

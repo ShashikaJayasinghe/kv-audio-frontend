07255d7a7dbd861d89f6b76a07e0e8adbabbceb6
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-02-22 14:24:25 +0530

    tailwindcss and basic dashboard implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import './App.css'
-import ProductCard from './components/productCard'
+import { BsGraphDown } from "react-icons/bs";
+import { FaRegBookmark, FaRegUser } from "react-icons/fa6";
+import { MdOutlineRateReview, MdSpeaker } from "react-icons/md";
 
 function App() {
 
   return (
-    <div>
-      <ProductCard name="JBL Charge Mini 3" price="3250/-" description="Portable Wireless Bluetooth Speaker With FM Radio JBL" photoURL="https://img.drz.lazcdn.com/g/kf/S6cfb0ee8d9f74ebb9660fd04b3622e364.jpg_720x720q80.jpg_.webp" />
-      <ProductCard name="Wireless Bluetooth Headphones" price="3900/-" description="P47 Bluetooth 4.1 Headphone Wireless Headband Earphone Hands Free Music Headset With MF/TF & AUX ports , P47 Cat Ears" photoURL="https://img.drz.lazcdn.com/static/lk/p/b2a509263c206e69a6aef620d2579aa2.jpg_720x720q80.jpg_.webp" />
+    <div className='w-full h-screen flex'>
+      <div className='w-[400px] h-full bg-green-200'>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'> <BsGraphDown />Dashboard</button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><FaRegBookmark />Bookings</button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><MdSpeaker />Items</button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><FaRegUser />Users</button>
+        <button className='w-full h-[40px] text-[25px] font-bold flex justify-center items-center'><MdOutlineRateReview />Reviews</button>
+      </div>
+      <div className='w-full bg-red-900'>
 
+      </div>
     </div>
   )
 }

3ee62b89cbf04d6da439e1233b780a81d6d23da9
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-02-23 13:22:35 +0530

    useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
 import AdminPage from './pages/admin/admin';
 import HomePage from './pages/home/homePage';
+import Testing from './components/testing';
 
 function App() {
   
   return (
     <BrowserRouter>
       <Routes path="/*">
+        <Route path='/testing' element={<Testing />}></Route>
         <Route path='/admin/*' element={<AdminPage />}/>
         <Route path='/*' element={<HomePage />}/>
       </Routes>

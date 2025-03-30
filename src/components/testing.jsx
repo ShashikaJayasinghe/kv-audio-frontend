1e01f5f827859e4499815461163ce63fb89393f1
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-03-30 14:05:20 +0530

    testing file implementation ok

--- src/components/testing.jsx
+++ src/components/testing.jsx
@@ -5,7 +5,7 @@ export default function Testing () {
 
     return(
         <div className="w-full h-screen flex flex-col justify-center items-center">
-            <input type="file" multiple onChange={(e)=>{console.log(e.target.files[0])}}/>
+            <input type="file" multiple onChange={(e)=>setFile(e.target.files[0])}/>
             <button className="w-[200px] h-[50px] bg-blue-500 text-secondary py-2 rounded-lg hover:bg-blue-600 transition">Uplaod</button>
         </div>
     )

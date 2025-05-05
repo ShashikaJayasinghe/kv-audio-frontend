802410b78e19195fcd0618e9d60cff3b9a313950
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-05-05 11:36:49 +0530

    media upload supabase 👍

--- src/utils/mediaUpload.jsx
+++ src/utils/mediaUpload.jsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js"
 const anon_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpic3ZzdXRmaW13eHRuZHhhbWdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MjE3NTMsImV4cCI6MjA1NzE5Nzc1M30.d00YwgDj76m-iZPTg8cOBaa2MXRcbRDCE8rwQSmcaFs"
 const supabase_url = "https://zbsvsutfimwxtndxamge.supabase.co"
 
-const supabase = createClient(supabase_url, anon_key)
+const supabase = createClient(supabase_url, anon_key)       // supabase url and anon key
 
 export default function mediaUpload (file) {
 
@@ -11,7 +11,7 @@ export default function mediaUpload (file) {
         if (file == null) {
             reject("No File Selected");
         }
-        const timestamp = new Date().getTime();
+        const timestamp = new Date().getTime();     
         const fileName = timestamp+file.name;
 
         supabase.storage.from("images").upload(fileName,file, {

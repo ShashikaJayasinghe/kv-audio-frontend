8b13f7435babd0734577cc0c1665bb3021305bb7
Shashika Jayasinghe <shashikajay72@gmail.com>
2025-05-07 22:12:14 +0530

    booking page a 👍

--- src/pages/home/bookingPage.jsx
+++ src/pages/home/bookingPage.jsx
@@ -7,7 +7,7 @@ import toast from "react-hot-toast";
 export default function BookingPage () {
     const [cart, setCart] = useState(loadCart());       //load cart
 
-    const today = formatDate(new Date());
+    const today = formatDate(new Date());       //   run formatDate funtion and get today date
     const tomorrow = formatDate(new Date(Date.now() + 24 * 60 * 60 * 1000));
 
     const [startingDate, setStartingDate] = useState(today);
